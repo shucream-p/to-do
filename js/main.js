09dafc8c0132cf5e6dba959584eb22fe7c312d87
tominaga <aurora25sh.50nonono@gmail.com>
2022-07-29 19:11:26 +0900

    削除機能を追加

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ const app = new Vue({
     addItem: function() {
       this.todos.push(this.newItem)
       this.newItem = ''
+    },
+    deleteItem(index) {
+      if (confirm('削除してよろしいですか？')) {
+        this.todos.splice(index, 1)
+      }
     }
   }
 })
